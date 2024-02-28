5865be501954b9234d1da515b29c4b2a6d0321b6
SinisaKuna <48476351+SinisaKuna@users.noreply.github.com>
2024-02-28 19:18:18 +0100

    HtmlToPdfConverter

--- prijavaRadnika/src/main/java/hr/betaSoft/tools/HtmlToPdfConverter.java
+++ prijavaRadnika/src/main/java/hr/betaSoft/tools/HtmlToPdfConverter.java
@@ -7,12 +7,12 @@ import com.itextpdf.text.Font;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerFontProvider;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
 import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -26,15 +26,11 @@ public class HtmlToPdfConverter {
         // Creating PDF document
         Document pdfDocument = new Document(PageSize.A4);
 
-
         // Initializing PdfWriter for writing PDF
         PdfWriter pdfWriter = PdfWriter.getInstance(pdfDocument, new FileOutputStream(pdfFilePath));
 
         // Opening the document for writing
-        String customFontPath = "font/Roboto-Medium.ttf";
         pdfDocument.open();
-        BaseFont customBaseFont = BaseFont.createFont(customFontPath, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        Font customFont = new Font(customBaseFont, 12);
 
         // Creating a new XMLWorkerHelper
         XMLWorkerHelper worker = XMLWorkerHelper.getInstance();
@@ -46,14 +42,14 @@ public class HtmlToPdfConverter {
         pdfDocument.close();
     }
 
-    public static void main(String[] args) {
-        try {
-            String htmlFilePath = "html/input.html";
-            String pdfFilePath = "pdf/output.pdf";
-            convertHtmlToPdf(htmlFilePath, pdfFilePath);
-            System.out.println("Conversion completed. PDF file is created at: " + pdfFilePath);
-        } catch (IOException | DocumentException e) {
-            e.printStackTrace();
-        }
+
+
+        public static void main(String[] args) throws DocumentException, IOException {
+        String htmlFilePath = "prijavaRadnika/html/input.html";
+        String pdfFilePath = "prijavaRadnika/pdf/output.pdf";
+        convertHtmlToPdf(htmlFilePath, pdfFilePath);
+        System.out.println("Conversion completed. PDF file is created at: " + pdfFilePath);
     }
+
+
 }
