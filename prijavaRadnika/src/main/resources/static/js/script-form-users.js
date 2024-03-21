3eaf3c9124d9eb61d83910eff2e5337932ba381a
SinisaKuna <48476351+SinisaKuna@users.noreply.github.com>
2024-03-21 08:09:57 +0100

    Small bug fix

--- prijavaRadnika/src/main/resources/static/js/script-form-users.js
+++ prijavaRadnika/src/main/resources/static/js/script-form-users.js
@@ -1,3 +1,44 @@
-function doNothing() {
-  // do nothing for now
+document.addEventListener("DOMContentLoaded", function() {
+     function enableHiddenInputs() {
+      var hiddenInputs = document.querySelectorAll('input[type="hidden"]');
+      hiddenInputs.forEach(function(input) {
+          input.disabled = false;
+      });
+  }
+
+
+
+function toggleDatePickButtons() {
+    setTimeout(function() {
+        var datePickContainers = document.querySelectorAll('.datepicker-container');
+        datePickContainers.forEach(function(container) {
+            var input = container.querySelector('.date-pick');
+            var clearButton = container.querySelector('.clear-date-button');
+            var datePickerButton = container.querySelector('.ui-datepicker-trigger');
+
+            if (!input) console.log("Input element not found in container:", container);
+            if (!clearButton) console.log("Clear button not found in container:", container);
+            if (!datePickerButton) console.log("Date picker button not found in container:", container);
+
+            if (!input || !clearButton || !datePickerButton) return; // Provjeravamo jesu li elementi pronađeni
+
+            // Provjeravamo prisutnost atributa disabled ili ako je atribut postavljen na "true"
+            if (input.disabled || input.getAttribute('disabled') === 'disabled' || input.getAttribute('disabled') === 'true') {
+                clearButton.disabled = true;
+                datePickerButton.disabled = true;
+            } else {
+                clearButton.disabled = false;
+                datePickerButton.disabled = false;
+            }
+        });
+    }, 100);
 }
+
+
+
+enableHiddenInputs();
+toggleDatePickButtons();
+
+
+
+});
